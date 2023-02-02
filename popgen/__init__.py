6e4933e415d45e16119f1928884def59e629aa2b
psferguson <petersferguson@gmail.com>
2023-02-01 18:50:14 -0600

    linting

--- popgen/__init__.py
+++ popgen/__init__.py
@@ -2,10 +2,9 @@
 """
 Module for dealing with Isochrones.
 """
-from .utils import get_iso_dir
-from .factory import isochroneFactory
-from .composite import CompositeIsochrone, Padova, Dotter
-from .parsec import Bressan2012, Marigo2017
-from .dartmouth import Dotter2008
-from .mesa import Dotter2016
-
+from popgen.composite import CompositeIsochrone, Dotter, Padova
+from popgen.dartmouth import Dotter2008
+from popgen.factory import isochroneFactory
+from popgen.mesa import Dotter2016
+from popgen.parsec import Bressan2012, Marigo2017
+from popgen.utils import get_iso_dir
